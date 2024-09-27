3e1e9b8b8fb31a3dd5b87477123300210c20f36e
wangbertha <wangbertha@gmail.com>
2024-09-27 14:28:03 -0500

    Create Authenticate button functionality

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,5 +1,33 @@
-export default function Authenticate() {
+import { useState } from "react"
+
+export default function Authenticate({ token }) {
+    const [successMessage, setSuccessMessage] = useState(null)
+    const [error, setError] = useState(null)
+
+    async function getAuthentication() {
+        try {
+            const API_URL = 'https://fsa-jwt-practice.herokuapp.com/authenticate';
+            const response = await fetch(API_URL, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json', 
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            const result = await response.json();
+            setSuccessMessage(result.message)
+        }
+        catch (error) {
+            setError(error.message);
+        }
+    }
+
   return (
-    <h2>Authenticate</h2>
+    <>
+        <h2>Authenticate</h2>
+        {successMessage ?? <p>{successMessage}</p>}
+        {error ?? <p>{error}</p>}
+        <button onClick={getAuthentication}>Authenticate Token</button>
+    </>
   )
 }

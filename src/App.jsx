d6eb30ef49f4814ed59a431779ec1bfed55d474f
wangbertha <wangbertha@gmail.com>
2024-09-27 14:27:35 -0500

    Pass props from App to SignUpForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import './App.css'
 import Authenticate from './components/Authenticate'
 import SignUpForm from './components/SignUpForm'
 
 function App() {
+  const [token, setToken] = useState(null)
+ ; 
   return (
     <>
-      <SignUpForm />
-      <Authenticate />
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
     </>
   )
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState(null);
@@ -16,7 +16,7 @@ export default function SignUpForm() {
                 body: JSON.stringify({ username, password}),
             })
             const result = await response.json();
-            console.log(result);
+            setToken(result.token);
         }
         catch (error) {
             setError(error.message);

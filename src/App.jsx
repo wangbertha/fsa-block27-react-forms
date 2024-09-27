fc6121fdd1278981b750ac3b16c1b790c5e87623
wangbertha <wangbertha@gmail.com>
2024-09-27 13:40:47 -0500

    Create empty SignUpForm and Authenticate comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
   return (
     <>
-      
+      <SignUpForm />
+      <Authenticate />
     </>
   )
 }
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,5 @@
+export default function SignUpForm() {
+  return (
+    <div>Sign Up</div>
+  )
+}
